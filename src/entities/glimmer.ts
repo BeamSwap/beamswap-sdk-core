a315835bdc031c93d7b9192c73d447b734a8d006
flisko <ziga.flis@student.um.si>
2023-06-01 12:36:32 +0200

    add glimmer

---
+++ src/entities/glimmer.ts
@@ -0,0 +1,32 @@
+import invariant from 'tiny-invariant'
+import { Currency } from './currency'
+import { NativeCurrency } from './nativeCurrency'
+import { Token } from './token'
+import { WETH9 } from './weth9'
+
+
+export class Glimmer extends NativeCurrency {
+
+  protected constructor(chainId: number) {
+    super(chainId, 18, 'GLMR', 'Glimmer')
+  }
+  public get wrapped(): Token {
+    const weth9 = WETH9[this.chainId]
+    invariant(!!weth9, 'WRAPPED')
+    return weth9
+  }
+
+  private static _cache: { [chainId: number]: Glimmer } = {}
+
+  public static onChain(chainId: number): Glimmer {
+    return this._cache[chainId] ?? (this._cache[chainId] = new Glimmer(chainId))
+  }
+
+  public equals(other: Currency): boolean {
+    return other.isNative && other.chainId === this.chainId
+  }
+
+  public sortsBefore(other: Token): boolean {
+    return false
+  }
+}
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,7 +1,9 @@
-export * from './fractions'
-
+export * from './Glimmer'
 export * from './currency'
 export * from './ether'
+export * from './fractions'
 export * from './nativeCurrency'
 export * from './token'
 export * from './weth9'
+
+
